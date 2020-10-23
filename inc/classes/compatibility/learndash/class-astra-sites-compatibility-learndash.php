1902c275f562c5e557e66c13fcb26d56a93c392f
Mahesh Waghmare <maheshw@bsf.io>
2020-10-23 14:54:46 +0530

    Version bump

--- inc/classes/compatibility/learndash/class-astra-sites-compatibility-learndash.php
+++ inc/classes/compatibility/learndash/class-astra-sites-compatibility-learndash.php
@@ -5,7 +5,7 @@
  * @see  https://wordpress.org/plugins/astra-pro/
  *
  * @package Astra Sites
- * @since x.x.x
+ * @since 2.3.8
  */
 
 // If LearnDash is not defined then return false.
@@ -18,7 +18,7 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_LearnDash' ) ) :
 	/**
 	 * Astra Sites Compatibility LearnDash
 	 *
-	 * @since x.x.x
+	 * @since 2.3.8
 	 */
 	class Astra_Sites_Compatibility_LearnDash {
 
@@ -27,14 +27,14 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_LearnDash' ) ) :
 		 *
 		 * @access private
 		 * @var object Class object.
-		 * @since x.x.x
+		 * @since 2.3.8
 		 */
 		private static $instance;
 
 		/**
 		 * Initiator
 		 *
-		 * @since x.x.x
+		 * @since 2.3.8
 		 * @return object initialized object of class.
 		 */
 		public static function get_instance() {
@@ -47,7 +47,7 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_LearnDash' ) ) :
 		/**
 		 * Constructor
 		 *
-		 * @since x.x.x
+		 * @since 2.3.8
 		 */
 		public function __construct() {
 			add_filter( 'astra_sites_pre_process_post_disable_content', '__return_false' );
