0d4f02388cde0557c989561a85dbf2f7222065ca
Amit <amith@bsf.io>
2021-05-26 11:19:07 +0530

    fix: quick links improvement

--- inc/lib/bsf-quick-links/class-bsf-quick-links.php
+++ inc/lib/bsf-quick-links/class-bsf-quick-links.php
@@ -96,12 +96,14 @@ if ( ! class_exists( 'BSF_Quick_Links' ) ) {
 
 			?>
 			<div class="bsf-quick-link-wrap">
-				<label class="bsf-quick-link-title"><?php echo esc_html( $data['default_logo']['title'] ); ?></label>
 				<div class="bsf-quick-link-items-wrap hide-wrapper">
 					<?php echo $this->get_links_html( $data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				</div>
 				<a href="#" class="bsf-quick-link">
-					<img src="<?php echo esc_url( $data['default_logo']['url'] ); ?>">
+					<div class="quick-link-button-wrap">
+						<span class="dashicons dashicons-admin-links"></span>
+						<span>Quick Links</span>
+					</div>
 				</a>
 			</div>
 			<?php
