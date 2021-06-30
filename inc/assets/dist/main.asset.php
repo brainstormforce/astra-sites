993172edc9824f221cb3257a85f3366c619f7511
Mahesh Waghmare <mwaghmare7@gmail.com>
2021-06-30 12:34:58 +0530

    Removed unwanted files.

--- inc/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '6ec0d8dce700a31638507d0d9bd61545');
